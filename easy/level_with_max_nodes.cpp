c8901f931cc3c121785eb71419fb774b7fd81c0d
Anand <40825655+anand-371@users.noreply.github.com>
2020-01-15 13:14:36 +0530

    Update level_with_max_nodes.cpp

--- easy/level_with_max_nodes.cpp
+++ easy/level_with_max_nodes.cpp
@@ -32,3 +32,23 @@ int maxNodeLevel(Node *root)
     }
     return level_no;
 }
+
+/*
+Sum of Leaf Nodes
+Given a Binary Tree of size N. The task is to complete the function sumLeaf(), that should return the sum of all the leaf nodes of the given binary tree.
+
+Input:
+First line of input contains number of testcases T. For each testcase, there will be two lines,
+first of which containing the number of edges (between two nodes) in the tree. Next line 
+contains N pairs (considering a and b) with a 'L' (means node b on left of a) or 'R' (means node b on right of a) after a and b.
+int sumLeaf(Node* root)
+{   if(root==NULL){return 0;}
+    if(root->left==NULL && root->right==NULL){
+        return root->data;
+    }
+    return sumLeaf(root->left)+sumLeaf(root->right);
+    
+}
+
+
+*/
