98b37fb4b90328f38c6342506f7ae35a40724b32
Anand <40825655+anand-371@users.noreply.github.com>
2020-04-17 02:15:32 +0530

    cpp template for macros

---
+++ standard_template.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#include<cmath>
+#define max(a,b) (a<b?b:a)
+#define min(a,b) (a<b?a:b)
+#define INF 1001001001
+#define ll long long 
+#define ull unsigned long long
+#define ui unsigned int
+#define us unsigned short
+#define print(a) cout<<a<<" "
+#define rep(i,n) for (int i = 0; i < n; ++i)
+#define whileZ int T; getI(T); while(T--)
+#define DBG(vari) cerr<<#vari<<" = "<<(vari)<<endl;
+#define getI(a) scanf("%d", &a) 
+#define getII(a,b) scanf("%d%d", &a, &b)
+#define getIII(a,b,c) scanf("%d%d%d", &a, &b, &c)
+int main(){
+std::ios_base::sync_with_stdio(false); 
+cin.tie(NULL);
+// code here ;)
+
+}
