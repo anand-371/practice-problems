001297f032254b3128aa9c60363f4b1b1061d5fb
Anand <40825655+anand-371@users.noreply.github.com>
2020-09-14 01:10:53 +0530

    search rotated array - ★★★

--- easy/search_rot_arr.cpp
+++ easy/search_rot_arr.cpp
@@ -5,7 +5,6 @@ Given an integer array nums sorted in ascending order, and an integer target.
 Suppose that nums is rotated at some pivot unknown to you beforehand (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
 
 You should search for target in nums and if you found return its index, otherwise return -1.
-
 Example 1:
 
 Input: nums = [4,5,6,7,0,1,2], target = 0
