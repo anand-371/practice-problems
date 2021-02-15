320321f7829ae5986e86129854c82b72853d0da4
Anand <40825655+anand-371@users.noreply.github.com>
2021-02-15 19:23:33 +0530

    Update and rename 3sum to 3sum.cpp

--- SDE problems/Day7/3sum.cpp
+++ SDE problems/Day7/3sum.cpp
@@ -1,5 +1,5 @@
 /*
-Given an array nums of n integers, are there elements a, b, c in
+Given an array nums of n integers, are there elements a, b, c 
 nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
 
 Notice that the solution set must not contain duplicate triplets.
