487c5ac93df1596f8aeabfecb703577c277ba5ac
Anand <40825655+anand-371@users.noreply.github.com>
2021-03-28 00:42:41 +0530

    Update and rename __IMP__perfecr_squares.cpp to __IMP__perfect_squares.cpp

--- medium/__IMP__perfect_squares.cpp
+++ medium/__IMP__perfect_squares.cpp
@@ -28,6 +28,37 @@ Output
 2
 Explanation
 9 + 9
+
+Intuition
+DP approach.
+
+dp[i] means how many perfect square numbers which sum to i. Hence, i is the upper limit.
+
+Either the current dp[i] or the previous one plus one.
+
+dp[0] = 0 
+dp[1] = dp[0]+1 = 1
+dp[2] = dp[1]+1 = 2
+dp[3] = dp[2]+1 = 3
+dp[4] = min(dp[4-1*1]+1, dp[4-2*2]+1) 
+      = min(dp[3]+1, dp[0]+1)
+      = 1				
+dp[5] = min(dp[5-1*1]+1, dp[5-2*2]+1) 
+      = min(dp[4]+1, dp[1]+1) 
+      = 2
+
+dp[n] = min(dp[n-i*i]+1),  n-i*i>=0&&i>=1
+int solve(int n) {
+    vector<int> dp(n + 1, 0);
+    for (int i = 1; i <= n; i++) {
+        dp[i] = i;
+        for (int j = 1; j * j <= i; j++) {
+            dp[i] = min(dp[i], dp[i - j * j] + 1);
+        }
+    }
+    return dp[n];
+}
+
 */
 
 int solve(int n) {
